3089925e3641d5d43a7f4c78148c8587fa4b0056
Florian Schweizer <post.an.flo@gmail.com>
2022-06-21 17:07:57 +0200

    Initial Commit

---
+++ NavStack/NavStackApp.swift
@@ -0,0 +1,12 @@
+// Created by Florian Schweizer on 21.06.22
+
+import SwiftUI
+
+@main
+struct NavStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
